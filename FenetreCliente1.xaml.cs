a2b767d9b3bf21a54179a971c004c32f4db3304c
stagiaire <stagiaire@PC-B-6-95R9J32>
2019-10-29 16:08:23 +0100

    Ajoutez des fichiers projet.

---
+++ FenetreCliente1.xaml.cs
@@ -0,0 +1,50 @@
+﻿using ClientpharmacieWPF.ServiceReference1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ClientpharmacieWPF
+{
+    /// <summary>
+    /// Logique d'interaction pour FenetreCliente1.xaml
+    /// </summary>
+    public partial class FenetreCliente1 : Window
+    {
+        Service1Client svc = new Service1Client();
+        List<ProduitReturn> listeStock;
+        private string _nom;
+        public FenetreCliente1(ClientReturn cli )
+        {
+            InitializeComponent();
+
+
+            var listeStock = svc.listeProduit();
+            listeBoxProduit.ItemsSource = listeStock; // ajout de la liste des produit dans le combobox
+            listeBoxProduit.DisplayMemberPath = "nom_produit_stock";//suite
+            listeBoxProduit.SelectedIndex = 0;//suite
+
+             _nom = cli.nom;
+            lab_nomClient.Text = cli.nom +" "+ cli.prenom; 
+            lab_titre.Content = cli.nom;
+        }
+
+        
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            string nomClient = _nom;
+            string nomProduit = listeBoxProduit.Text;
+            string quantite = txtquantite_cmd.Text;
+            svc.passerCommande(nomClient, nomProduit, Convert.ToInt32(quantite));
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,62 @@
+﻿using ClientpharmacieWPF.ServiceReference1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ClientpharmacieWPF
+{
+    /// <summary>
+    /// Logique d'interaction pour MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Service1Client svc = new Service1Client();
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnInscription_Click(object sender, RoutedEventArgs e)
+        {
+            svc.ajouterclients(txtNom.Text, txtPrenom.Text, txtEmail.Text, txtPassword.Password);
+            //vider(); 
+            MessageBox.Show("Ajouter avec succes!");
+        }
+
+        private void btnConnexion_Click(object sender, RoutedEventArgs e)
+        {
+            if (string.IsNullOrEmpty(conEmail.Text) || string.IsNullOrEmpty(conPassword.Password))
+            {
+                error.Content = "Champ vide !!!";
+               // vider();
+            }
+            else
+            {
+                bool trouve = svc.verifierClients(conEmail.Text, conPassword.Password);
+                if (trouve)
+                {
+                    ClientReturn cli = svc.recupereParEmail(conEmail.Text);
+
+                    FenetreCliente1 fenetre1 = new FenetreCliente1(cli);
+                    fenetre1.Title = cli.nom;
+                    fenetre1.Show();
+                    this.Close();
+                }
+                else
+                    error.Content = "Erreur !!!";
+               // vider();
+            }
+        }
+    }
+}
