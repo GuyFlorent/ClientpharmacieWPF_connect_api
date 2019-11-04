433c8923041251a657bacab075c321f5c984318d
stagiaire <stagiaire@PC-B-6-95R9J32>
2019-11-04 17:01:45 +0100

    bon  il manque actualliser le client

--- FenetreCliente1.xaml.cs
+++ FenetreCliente1.xaml.cs
@@ -92,7 +92,7 @@ namespace ClientpharmacieWPF
             txtNom.Text = this.client1.nom;
             var listeOrder = svc.getcommandehisto(this.client1).OrderByDescending(f=> f.heureCommand);
             var prix_textb = listeOrder.FirstOrDefault(s => s.nom_Produit == listeBoxProduit.SelectedItem.ToString());
-            tb_prix_unité.Text = prix_textb.prix_Produit_Unite.ToString();
+          //  tb_prix_unité.Text = prix_textb.prix_Produit_Unite.ToString();
 
             maListeBox.DataContext = listeOrder;
             
